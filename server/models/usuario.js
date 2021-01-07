7c183b9225518f1871ec8e09aa77d8deec91719e
Sebas <sebastob2000@gmail.com>
2021-01-06 21:53:37 -0500

    añadiendo la función post y put

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
+//variable para validar valores únicos como el email
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    //mensaje de error
+    message: '{VALUE} no es un rol válido'
+};
+
 let Schema = mongoose.Schema;
 
 let usuarioSchema = new Schema({
@@ -10,6 +19,8 @@ let usuarioSchema = new Schema({
     },
     email: {
         type: String,
+        //debe de estar esto para poder utilizar la variable  uniqueValidator
+        unique: true,
         required: [true, 'El correo es necesario']
     },
     password: {
@@ -22,7 +33,9 @@ let usuarioSchema = new Schema({
     },
     role: {
         type: String,
-        default: 'USER_ROLE'
+        default: 'USER_ROLE',
+        //esto evalúa que el rol enviado si sea uno de los dos que declaramos en el objeto rolesValidos
+        enum: rolesValidos
     },
     estado: {
         type: Boolean,
@@ -33,6 +46,17 @@ let usuarioSchema = new Schema({
         default: false
     }
 });
+
+//con esto no se imprimirá la contraseña ni la key de ese objeto llamada 'password' en el archivo routes/usuario.js
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+}
+
+//con esto le podemos poner un mensaje para que aparezca al mandar un email que ya esté en la DB
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
 
 //para exportar este modelo
 module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,4 +1,12 @@
 const express = require('express');
+
+//para poder encriptar las contraseñas
+const bcrypt = require('bcrypt');
+
+//para que en el put al actualizar retorne solo los valores que yo quiera que se pueden actualizar
+const _ = require('underscore');
+
+//el modelo de la base de datos models/usuario.js
 const Usuario = require('../models/usuario');
 
 const app = express();
@@ -9,17 +17,19 @@ app.get('/usuario', (req, res) => {
     res.json('get usuario local');
 });
 
-// para crear nuevos registros
+//-----------------------------------------para crear nuevos registros
 app.post('/usuario', (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: body.password,
+        // con esto se encripta la contraseña
+        password: bcrypt.hashSync(body.password, 10),
         role: body.role
     });
 
+    //guarda el registro en la DB
     usuario.save((err, usuarioDB) => {
         if (err) {
             return res.status(400).json({
@@ -28,35 +38,40 @@ app.post('/usuario', (req, res) => {
             });
         }
 
+        // usuarioDB.password = null;
+
         res.json({
             ok: true,
             usuario: usuarioDB
 
         });
     });
-
-    // if (body.nombre === undefined) {
-    //     res.status(400).json({
-    //         ok: false,
-    //         mensaje: 'El nombre es necesario'
-    //     });
-    // } else {
-    //     res.json({
-    //         persona: body
-    //     });
-    // }
 });
 
 
-//put es para actualizar registros
+//--------------------------------put es para actualizar registros
 app.put('/usuario/:id', (req, res) => {
+    //se coge el id porque con ese se buscará para actualizarlo
     let id = req.params.id;
-    res.json({
-        id
+
+    //el body es lo que va a ser actualizado
+    //la función pick es para poner los valores del modelo que si serán permitidos actualizar
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
     });
 });
 
-// delete
+//--------------------------------delete
 app.delete('/usuario', (req, res) => {
     res.json('delete usuario');
 });
